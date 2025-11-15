3e2b29dbbaecaf25591814e17a7388d348e19c0f
Kunal Jambhale <jambhalekunal06@gmail.com>
2025-11-15 15:22:27 +0530

    created user model

---
+++ src/models/userModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        reqired: [true, "Username is required"],
+        unique: [true,"Username already exists"],
+    },
+
+    email: {
+        type: String,
+        reqired: [true, "Email is required"],
+        unique: [true,"Email already exists"],
+    },
+
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date
+
+});
