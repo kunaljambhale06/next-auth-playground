e9dfaedce9879525918e0075ce955256467f1a93
Kunal Jambhale <jambhalekunal06@gmail.com>
2025-11-16 14:42:16 +0530

    Initial commit and error handling in dbConfig

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,2 +1,8 @@
 import mongoose from "mongoose";
 
+export async function connectToDatabase() {
+    try {
+        mongoose.connect(process.env.MONGODB_URI)
+    } catch (error) {
+        console.log("Database connection error:", error);
+    }
