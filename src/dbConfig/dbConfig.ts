467771460c491db089eafb2fa222469b81b80ec3
Kunal Jambhale <jambhalekunal06@gmail.com>
2025-11-16 19:01:38 +0530

    Initialized helper folder for nodemailer

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -2,7 +2,19 @@ import mongoose from "mongoose";
 
 export async function connectToDatabase() {
     try {
-        mongoose.connect(process.env.MONGODB_URI)
+        mongoose.connect(process.env.MONGODB_URI!)
+
+        const connection = mongoose.connection;
+
+        connection.on('connected', () => {
+            console.log("Database connected successfully");
+        })
+
+        connection.on('error',(err) => {
+            console.log("Database connection failed" + err);
+            process.exit();
+            
+        } 
     } catch (error) {
         console.log("Database connection error:", error);
     }
