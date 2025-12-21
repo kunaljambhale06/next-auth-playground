0ebb416dc2008633a0674704dbf0b2fb0cca7c63
Kunal Jambhale <jambhalekunal06@gmail.com>
2025-12-21 15:16:56 +0530

    Initialized and updated logout route

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,47 @@
+import { connect, connectToDatabase } from '@/dbConfig/dbConfig'
+import User from '@/models/userModel'
+import { NextRequest, NextResponse } from 'next/server';
+import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+connectToDatabase();
+export async function POST(request: NextRequest) {
+    try {
+        const reqBody = await request.json();
+        const { email, password } = reqBody;
+        console.log(reqBody);
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return NextResponse.json({ message: 'User does not exist' }, { status: 400 })
+        }
+        console.log("User exists:", user);
+        const validPassword = await bcryptjs.compare(password, user.password);
+
+        if (!validPassword) {
+            return NextResponse.json({ message: 'Check your credentials' }, { status: 400 })
+        } 
+
+        //GENERATING JWT TOKEN
+        const tokenData = {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+
+        const token = jwt.sign(tokenData, process.env.JWT_SECRET_KEY!, { expiresIn: '1d' })
+
+        const responce = NextResponse.json({
+            message: "Logged in successfully",
+            success: true,
+        })
+        responce.cookies.set('token', token, {
+            httpOnly: true,// USER CANNOT ACCESS THIS COOKIE VIA JAVASCRIPT
+        })
+        return responce;
+
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 })
+
+    }
+}
