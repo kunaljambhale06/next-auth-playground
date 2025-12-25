d9fcf2b8fe959fb12b17a27a52426fea7a8b508c
Kunal Jambhale <jambhalekunal06@gmail.com>
2025-12-25 19:40:48 +0530

    Refactor getDataFromToken to use jwt.verify

--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -1,11 +1,16 @@
 import { NextRequest } from "next/server";
-import { Jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 
 export const getDataFromToken = (request: NextRequest) => {
     try {
+        const token = request.cookies.get("token")?.value || "";
+        const decodedToken:any = jwt.verify(token , process.env.JWT_TOKEN_SECRET!)
+
+        return decodedToken.id //THIS id IS FROM tokenData IN WHICH WE USED id: _id
         
-    } catch (error) {
-        
+
+    } catch (error: any) {
+        throw new Error(error.message)
     }
 }
