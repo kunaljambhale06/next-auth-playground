3ab784be7aac95db8bc192f5f9ca32756c91e7dc
Kunal Jambhale <jambhalekunal06@gmail.com>
2026-01-05 19:04:36 +0530

    Updated signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -10,9 +10,18 @@ export default function SignupPage() {
 
   const [buttonDisabled, setButtonDisabled] = useState(false)
   const [loading, setLoading] = useState(false)
-  
+
+  const onSignup = () => {
+    try {
+      
+    } catch (error : any) {
+      console.log("Signup failed", error.message);
+    }
   return (
     <div>SignUp Page</div>
   )
 }
+
+}
+
 
