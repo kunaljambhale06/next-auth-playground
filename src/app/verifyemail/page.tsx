750f2758ffc98ec4c862b1f2f20e97efa319edfb
Kunal Jambhale <jambhalekunal06@gmail.com>
2026-01-07 13:29:03 +0530

    Fixed some typo in verify email page

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -1,11 +1,56 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { error } from 'console'
 
 export default function VerifyEmailPage() {
     const [token, setToken] = useState("")
     const [verified, setVerified] = useState(false)
-    
+    const [error, setError] = useState(false)
+
+    const verifyUserEmail = async () => {
+        try {
+            await axios.post("/api/users/verifyemail", {token})
+            setVerified(true)
+            setError(false)
+        } catch (error: any) {
+            setError(true)
+            console.log(error.responce.data)
+            
+        }
+    }
+
+    useEffect(() => {
+        setError(false)
+        const urlToken = window.location.search.split("=")[1]
+        setToken(urlToken || "")
+    }, [])
+
+    useEffect(() => {
+        setError(false)
+        if (token.length > 0) {
+            verifyUserEmail()
+        }
+    }, [token])
   return (
-    <div>Verify Email</div>
+    <div className='flex flex-col items-center justify-center
+    min-h-screen py-2'>
+        <h1 className='text-4xl'>Verify Your Email</h1>
+        <h2 className='p-2 bg-orange-500 text-black'>
+            {token ? `${token}` : "No Token"}
+        </h2>
+        {verified && (
+            <div>
+                <h2>Verified Successfully</h2>
+                <Link href="/login">Login To your account</Link>
+            </div>
+        )}
+        {error && (
+            <div>
+                <h2>Error Verifying the Email</h2>
+                <Link href="/login">Login To your account</Link>
+            </div>
+        )}
+    </div>
   )
 }
