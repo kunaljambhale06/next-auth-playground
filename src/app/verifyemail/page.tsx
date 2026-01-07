25ab9416693e17f38e11d73ab2b92a27ae3036d8
Kunal Jambhale <jambhalekunal06@gmail.com>
2026-01-07 13:25:16 +0530

    Updated Verify Email Page

---
+++ src/app/verifyemail/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React, { useState } from 'react'
+
+export default function VerifyEmailPage() {
+    const [token, setToken] = useState("")
+    const [verified, setVerified] = useState(false)
+    
+  return (
+    <div>Verify Email</div>
+  )
+}
