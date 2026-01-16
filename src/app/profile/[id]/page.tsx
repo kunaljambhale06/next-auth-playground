019151bd7d1267db9554e222de597c4fd04ff233
Kunal Jambhale <jambhalekunal06@gmail.com>
2026-01-16 18:26:37 +0530

    Initialized middleware

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -6,7 +6,6 @@ export default function page({params}: any) {
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <h1>Profile Page</h1>
         <h2 className="p-3 bg-green-500 rounded text-black">{params.id}</h2>
-        
     </div>
   )
 }
