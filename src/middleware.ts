b266cf4c1dd4f76c8a6684129b2c1a37c6b1741b
Kunal Jambhale <jambhalekunal06@gmail.com>
2026-01-18 09:13:02 +0530

    Added middleware for private routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,30 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware 
+export function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname
+
+    const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail'
+
+    const token = request.cookies.get("token")?.value || ''
+
+    //TODO IN THIS REDIRECT CHECK THE PATH WHERE WE ARE TRYING TO REDIRECT OUR USER
+
+    if (isPublicPath && token) {
+        return NextResponse.redirect(new URL('/me', request.url))
+    }
+
+    if (!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
+}
+
+export const config = {
+    matcher: [
+        '/',
+        '/login',
+        '/signup',
+        '/profile',
+        '/verifyemail'
+    ]
+}
