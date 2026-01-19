957c757ee18718ef2b643b115d59a02b86ac660b
Kunal Jambhale <jambhalekunal06@gmail.com>
2026-01-19 12:22:49 +0530

    Updated login page design

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,95 @@
-import React from 'react'
+'use client';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import toast from 'react-hot-toast'
+import { useRouter } from 'next/navigation'
+import Link from 'next/link';
 
-function page() {
+export default function SignupPage() {
+  const router = useRouter()
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: ""
+  })
+
+  const [buttonDisabled, setButtonDisabled] = useState(true)
+  const [loading, setLoading] = useState(false)
+
+  const onSignup = async () => {
+    try {
+      setLoading(true)
+      const response = await axios.post("/api/users/signup", user)
+      console.log("Sign-Up Successful", response.data)
+      toast.success("Sign-Up Successful ! Please verify your email.")
+      router.push("/verifyemail")
+
+
+    } catch (error: any) {
+      console.log("Signup failed", error.message)
+      toast.error(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setButtonDisabled(false)
+    } else {
+      setButtonDisabled(true)
+    }
+  }, [user])
+
   return (
-    <div>Login Page</div>
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <h1>{loading ? "Processing" : "Sign Up"}</h1>
+      <hr />
+
+      <label htmlFor="username">username</label>
+      <input
+        className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black'
+        id='username'
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder='Username'
+        type="text"
+      />
+
+      <label htmlFor="email">email</label>
+      <input
+        className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black'
+        id='email'
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder='Email'
+        type="email"
+      />
+
+      <label htmlFor="password">password</label>
+      <input
+        className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black'
+        id='password'
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder='Password'
+        type="password"
+      />
+
+      <button
+        onClick={onSignup}
+        disabled={buttonDisabled}
+        className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 disabled:opacity-50'
+      >
+        {buttonDisabled ? "Kindly fill all fields" : "Sign Up"}
+      </button>
+
+      <Link href="/login">Visit Login Page</Link>
+    </div>
   )
 }
-
-export default page
