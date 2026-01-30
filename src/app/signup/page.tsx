1ab65f9aeed2c7c5f1b50db36192415171e8a5ff
Kunal Jambhale <jambhalekunal06@gmail.com>
2026-01-30 17:08:39 +0530

    Updated backgroud colour of signpu page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -47,7 +47,7 @@ export default function SignupPage() {
   }, [user])
 
   return (
-  <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
+  <div className="min-h-screen flex items-center justify-center bg-slate-50 px-4">
 
     <div className="max-w-sm bg-white px-6 py-8 rounded-lg shadow-md">
 
