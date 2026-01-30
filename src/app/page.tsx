4ec60e2faf9721dad252cc33ef1c944db9bc22f7
Kunal Jambhale <jambhalekunal06@gmail.com>
2026-01-30 13:28:02 +0530

    Updated the style of description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,10 @@ export default function LandingPage() {
         <h1 className="text-3xl font-bold mb-4 text-gray-800">
           Welcome To Complete Next JS authentication system
         </h1>
-        <p className="text-gray-600 mb-6">
-This project showcases a complete authentication workflow using Next.js App Router, JWT-based sessions, and MongoDB.
-It focuses on secure login, signup, route protection, and real-world authentication patterns.        </p>
+        <p className="text-gray-800 mb-6">
+          This project showcases a complete authentication workflow using Next.js App Router, JWT-based sessions, and MongoDB.
+          It focuses on secure login, signup, route protection, and real-world authentication patterns.        
+        </p>
         <div className="flex justify-center gap-8">
           <button
             onClick={() => router.push("/login")}
