d2c8d354a8759a59740d00d4efa293ced5c75398
Kunal Jambhale <jambhalekunal06@gmail.com>
2026-02-11 17:09:22 +0530

    Added todo for user's data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 "use client"; 
 import { useRouter } from "next/navigation"; 
 
+//TODO : FIX THE TYPE OF DATA USER CAN ADD AND CAN VIEW
+  
+
 export default function LandingPage() {
   const router = useRouter();
 
